d2994cd9593f361f614e9ccb086fa1ad64171f60
Matthew Perry <perrygeo@gmail.com>
2017-07-17 16:09:48 -0600

    use WatchbotProgress object to encapsulate AWS resources

--- watchbot_progress/__init__.py
+++ watchbot_progress/__init__.py
@@ -10,125 +10,129 @@ import uuid
 import boto3
 
 
-# TODO remove or check at funcion invokation, not import
-TABLE = os.environ.get(
-    'ProgressTable', 'arn:aws:dynamodb:us-east-1:234858372212:table/pxm-test-Watchbot-progress')
-TABLENAME = TABLE.split(':table/')[-1]  # just name
-TOPIC = os.environ.get(
-    'WorkTopic', 'arn:aws:sns:us-east-1:234858372212:pxm-test')
+class WatchbotProgress(object):
+    """Sets up objects for reduce mode job tracking with SNS and DynamoDB
 
-dynamodb = boto3.resource('dynamodb')
-db = dynamodb.Table(TABLENAME)
-sns = boto3.client('sns')
-
-
-def status(jobid, part=None):
-    '''get status from dynamodb
-
-    Parameters
-    ----------
-    jobid: string?
-    part: optional int
-
-    Returns
-    -------
-    dict
-    Based on the watchbot-progress.status
-    '''
-    print(f'DB get status of {jobid}')
-
-    res = db.get_item(
-        Key={'id': jobid}, ConsistentRead=True)
-    item = res['Item']
-    remaining = len(item['parts']) if 'parts' in item else 0
-    percent = (item['total'] - remaining) / item['total']
-
-    data = {'progress': percent}
-
-    if 'error' in item:
-        # failure must have a 'failed' key
-        data['failed'] = item['error']
-    if 'metadata' in item:
-        data['metadata'] = item['metadata']
-    if 'reduceSent' in item:
-        data['reduceSent'] = item['reduceSent']
-
-    if part is not None:
-        # js implementation
-        # if (part) response.partComplete =
-        # item.parts ? item.parts.values.indexOf(part) === -1 : true;
-        raise NotImplementedError()  # todo
-
-    return data
-
-
-def set_total(jobid, parts):
-    ''' set total number of parts for the job
-
-    Based on watchbot-progress.setTotal
-    '''
-    total = len(parts)
-    print(f'DB set total for {jobid} to {total}')
-    return db.update_item(
-        Key={
-            'id': jobid},
-        ExpressionAttributeNames={
-            '#p': 'parts',
-            '#t': 'total'},
-        ExpressionAttributeValues={
-            ':p': set(range(total)),
-            ':t': total},
-        UpdateExpression='set #p = :p, #t = :t')
-
-
-def fail_job(jobid, reason):
-    '''fail the job, notify dynamodb
-
-    Based on watchbot-progress.failJob
-    '''
-    print(f'DB job {jobid} failed because {reason}.')
-    db.update_item(
-        Key={
-            'id': jobid},
-        ExpressionAttributeNames={
-            '#e': 'error'},
-        ExpressionAttributeValues={
-            ':e': reason},
-        UpdateExpression='set #e = :e')
-
-
-def complete_part(jobid, partid):
-    print(f'DB part {partid} is done')
-    res = db.update_item(
-        Key={
-            'id': jobid},
-        ExpressionAttributeNames={
-            '#p': 'parts'},
-        ExpressionAttributeValues={
-            ':p': set([partid])},
-        UpdateExpression='delete #p :p',
-        ReturnValues='ALL_NEW')
-
-    record = res['Attributes']
-    if 'parts' in record and len(record['parts']) > 0:
-        complete = False
-    else:
-        complete = True
-    return complete
-
-
-def set_metadata(jobid, metadata):
-    """Associate arbitrary metadata with a particular map-reduce job
+    The methods of this object are based on the on the equivalent
+    methods in the JavaScript implementation:
+    https://github.com/mapbox/watchbot-progress
     """
-    raise NotImplementedError()
 
+    def __init__(self, table_arn=None, topic_arn=None):
+        # SNS Messages
+        self.sns = boto3.client('sns')
+        self.topic = topic_arn if topic_arn else os.environ['WorkTopic']
 
-def send_message(message, subject):
-    """Function wrapper to facilitate partial application"""
-    return sns.publish(
-        Message=json.dumps(message),
-        Subject=subject,
-        TopicArn=TOPIC)
+        # DynamoDB
+        self.table_arn = table_arn if table_arn else os.environ['ProgressTable']
+        if self.table_arn:
+            self.table = self.table_arn.split(':table/')[-1]  # just name
+        self.dynamodb = boto3.resource('dynamodb')
+        self.db = self.dynamodb.Table(self.table)
+
+    def status(self, jobid, part=None):
+        """get status from dynamodb
+
+        Parameters
+        ----------
+        jobid: string?
+        part: optional int
+
+        Returns
+        -------
+        dict, similar to JS watchbot-progress.status object
+        """
+        print(f'DB get status of {jobid}')
+
+        res = self.db.get_item(
+            Key={'id': jobid}, ConsistentRead=True)
+        item = res['Item']
+        remaining = len(item['parts']) if 'parts' in item else 0
+        percent = (item['total'] - remaining) / item['total']
+
+        data = {'progress': percent}
+
+        if 'error' in item:
+            # failure must have a 'failed' key
+            data['failed'] = item['error']
+        if 'metadata' in item:
+            data['metadata'] = item['metadata']
+        if 'reduceSent' in item:
+            data['reduceSent'] = item['reduceSent']
+
+        if part is not None:
+            # js implementation
+            # if (part) response.partComplete =
+            # item.parts ? item.parts.values.indexOf(part) === -1 : true;
+            raise NotImplementedError()  # todo
+
+        return data
+
+    def set_total(self, jobid, parts):
+        """ set total number of parts for the job
+
+        Based on watchbot-progress.setTotal
+        """
+        total = len(parts)
+        print(f'DB set total for {jobid} to {total}')
+        return self.db.update_item(
+            Key={
+                'id': jobid},
+            ExpressionAttributeNames={
+                '#p': 'parts',
+                '#t': 'total'},
+            ExpressionAttributeValues={
+                ':p': set(range(total)),
+                ':t': total},
+            UpdateExpression='set #p = :p, #t = :t')
+
+    def fail_job(self, jobid, reason):
+        """fail the job, notify dynamodb
+
+        Based on watchbot-progress.failJob
+        """
+        print(f'DB job {jobid} failed because {reason}.')
+        self.db.update_item(
+            Key={
+                'id': jobid},
+            ExpressionAttributeNames={
+                '#e': 'error'},
+            ExpressionAttributeValues={
+                ':e': reason},
+            UpdateExpression='set #e = :e')
+
+    def complete_part(self, jobid, partid):
+        """Mark part as complete
+        """
+        print(f'DB part {partid} is done')
+        res = self.db.update_item(
+            Key={
+                'id': jobid},
+            ExpressionAttributeNames={
+                '#p': 'parts'},
+            ExpressionAttributeValues={
+                ':p': set([partid])},
+            UpdateExpression='delete #p :p',
+            ReturnValues='ALL_NEW')
+
+        record = res['Attributes']
+        if 'parts' in record and len(record['parts']) > 0:
+            complete = False
+        else:
+            complete = True
+        return complete
+
+    def set_metadata(self, jobid, metadata):
+        """Associate arbitrary metadata with a particular map-reduce job
+        """
+        raise NotImplementedError()
+
+    def send_message(self, message, subject):
+        """Function wrapper to facilitate partial application"""
+        return self.sns.publish(
+            Message=json.dumps(message),
+            Subject=subject,
+            TopicArn=self.topic)
 
 
 #
@@ -136,17 +140,23 @@ def send_message(message, subject):
 #
 
 
-def create_job(parts, jobid=None, table=None, workers=8):
+def create_job(parts, jobid=None, workers=8, table_arn=None, topic_arn=None):
+    """Create a reduce mode job
+
+    Handles all the details of reduce-mode accounting (SNS, partid and jobid)
+    """
+
     jobid = jobid if jobid else str(uuid.uuid4())
 
-    set_total(jobid, parts)
+    progress = WatchbotProgress(table_arn=table_arn, topic_arn=topic_arn)
+    progress.set_total(jobid, parts)
 
     annotated_parts = [
         dict(**part, partid=partid, jobid=jobid)
         for partid, part in enumerate(parts)]
 
-    _send_message = partial(send_message, subject='map')
-
+    # Send SNS message for each part, concurrently
+    _send_message = partial(progress.send_message, subject='map')
     with futures.ThreadPoolExecutor(max_workers=workers) as executor:
         executor.map(_send_message, annotated_parts)
 
@@ -154,24 +164,28 @@ def create_job(parts, jobid=None, table=None, workers=8):
 
 
 @contextmanager
-def Part(jobid, partid, **kwargs):
+def Part(jobid, partid, table_arn=None, topic_arn=None, **kwargs):
     """Context Manager for parts of an ecs-watchbot reduce job.
 
     Params
     ------
     jobid
     partid
+    table_arn
+    topic_arn
+
     """
-    if 'failed' in status(jobid):
+    progress = WatchbotProgress(table_arn=table_arn, topic_arn=topic_arn)
+
+    if 'failed' in progress.status(jobid):
         raise RuntimeError(f'job {jobid} already failed')
 
     try:
-        # yield control to the context block which processes the message but
-        # doesn't need to know anything about the details of reduce mode
+        # yield control to the context block which processes the message
         yield
     except:
-        fail_job(jobid, partid)
+        progress.fail_job(jobid, partid)
     else:
-        all_done = complete_part(jobid, partid)
+        all_done = progress.complete_part(jobid, partid)
         if all_done:
-            send_message({'jobid': jobid}, 'reduce')
+            progress.send_message({'jobid': jobid}, subject='reduce')
