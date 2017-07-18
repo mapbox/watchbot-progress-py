4a5073eb1f695746763d8d0539b6c4c7fd3e00bd
Matthew Perry <perrygeo@gmail.com>
2017-07-18 13:23:58 -0600

    add tests, require pytest-cov

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup, find_packages
 
-setup(name='watchbot_progress',
-      version='0.1',
-      description=u"Watchbot reduce-mode helpers for python",
-      # long_description=long_description,
-      classifiers=[],
-      keywords='',
-      author=u"Matthew Perry",
-      author_email='perry@mapbox.com',
-      url='https://github.com/mapbox/watchbot-progress-py',
-      license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      install_requires=['boto3'],
-      include_package_data=True,
-      zip_safe=False)
+setup(
+    name='watchbot_progress',
+    version='0.1',
+    description=u"Watchbot reduce-mode helpers for python",
+    long_description='See https://github.com/mapbox/watchbot-progress-py',
+    classifiers=[],
+    keywords='',
+    author=u"Matthew Perry",
+    author_email='perry@mapbox.com',
+    url='https://github.com/mapbox/watchbot-progress-py',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    install_requires=['boto3'],
+    extras_require={
+        'test': ['pytest', 'pytest-cov']},
+    include_package_data=True,
+    zip_safe=False)
---
+++ tests/test_watchbot_progress.py
@@ -0,0 +1,94 @@
+import pytest
+from watchbot_progress import WatchbotProgress
+from unittest.mock import patch
+
+parts = [
+    {'source': 'a.tif'},
+    {'source': 'b.tif'},
+    {'source': 'c.tif'}]
+
+
+@patch('watchbot_progress.boto3.resource')
+def test_status(client, monkeypatch):
+    monkeypatch.setenv('WorkTopic', 'abc123')
+    monkeypatch.setenv('ProgressTable', 'arn::table/foo')
+
+    item = {'Item': {'total': 4, 'error': True}}
+    client.return_value.Table.return_value.get_item.return_value = item
+    s = WatchbotProgress().status('123')
+    assert s['failed']
+
+    item = {'Item': {'total': 4, 'metadata': True}}
+    client.return_value.Table.return_value.get_item.return_value = item
+    s = WatchbotProgress().status('123')
+    assert s['metadata']
+
+    item = {'Item': {'total': 4, 'reduceSent': True}}
+    client.return_value.Table.return_value.get_item.return_value = item
+    s = WatchbotProgress().status('123')
+    assert s['reduceSent']
+
+
+@patch('watchbot_progress.boto3.resource')
+@pytest.mark.parametrize('mock_item, expected', [
+    ({'Item': {'parts': [0, 1, 2, 3], 'total': 4}}, 0),
+    ({'Item': {'parts': [2, 3], 'total': 4}}, 0.5),
+    ({'Item': {'parts': [], 'total': 4}}, 1),
+    ({'Item': {'total': 4}}, 1)
+])
+def test_status_progress(client, mock_item, expected, monkeypatch):
+    monkeypatch.setenv('WorkTopic', 'abc123')
+    monkeypatch.setenv('ProgressTable', 'arn::table/foo')
+    client.return_value.Table.return_value.get_item.return_value = mock_item
+
+    s = WatchbotProgress().status('123')
+    assert s['progress'] == expected
+
+
+@patch('watchbot_progress.boto3.resource')
+def test_set_total(client, monkeypatch):
+    monkeypatch.setenv('WorkTopic', 'abc123')
+    monkeypatch.setenv('ProgressTable', 'arn::table/foo')
+    client.return_value.Table.return_value.update_item.return_value = None
+
+    WatchbotProgress().set_total('123', parts)
+
+
+@patch('watchbot_progress.boto3.resource')
+def test_fail_job(client, monkeypatch):
+    monkeypatch.setenv('WorkTopic', 'abc123')
+    monkeypatch.setenv('ProgressTable', 'arn::table/foo')
+    client.return_value.Table.return_value.update_item.return_value = None
+
+    WatchbotProgress().fail_job('123', 'Failed because it is bad')
+
+
+@patch('watchbot_progress.boto3.client')
+def test_send_message(client, monkeypatch):
+    monkeypatch.setenv('WorkTopic', 'abc123')
+    monkeypatch.setenv('ProgressTable', 'arn::table/foo')
+    client.return_value.publish.return_value = None
+
+    WatchbotProgress().send_message('I brought you this message', 'oh hai')
+
+
+@patch('watchbot_progress.boto3.resource')
+def test_complete_part(client, monkeypatch):
+    monkeypatch.setenv('WorkTopic', 'abc123')
+    monkeypatch.setenv('ProgressTable', 'arn::table/foo')
+
+    item = {'Attributes': {'parts': [], 'total': 4}}
+    client.return_value.Table.return_value.update_item.return_value = item
+    s = WatchbotProgress().complete_part('123', 1)
+    assert s is True
+
+
+@patch('watchbot_progress.boto3.resource')
+def test_incomplete_part(client, monkeypatch):
+    monkeypatch.setenv('WorkTopic', 'abc123')
+    monkeypatch.setenv('ProgressTable', 'arn::table/foo')
+
+    item = {'Attributes': {'parts': [1, 2], 'total': 4}}
+    client.return_value.Table.return_value.update_item.return_value = item
+    s = WatchbotProgress().complete_part('123', 1)
+    assert s is False
